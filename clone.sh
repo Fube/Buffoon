135466dd7611a12a889342faf93c39ca8b2d1a06
Fube <fubeitch@gmail.com>
2021-03-07 22:54:22 -0500

    Matching clone, ignore, and compose

--- clone.sh
+++ clone.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/Fube/homework_ws_buffoon
-git clone https://github.com/Fube/homework_ws_buffoon_rating_service
-git clone https://github.com/Fube/homework_ws_buffoon_user_service
-git clone https://github.com/Fube/homework_ws_buffoon_frontend
-git clone https://github.com/Fube/homework_ws_buffoon_backed/
+git clone https://github.com/Fube/homework_ws_buffoon JokeService
+git clone https://github.com/Fube/homework_ws_buffoon_rating_service RatingService
+git clone https://github.com/Fube/homework_ws_buffoon_user_service UserService
+git clone https://github.com/Fube/homework_ws_buffoon_frontend Frontend
+git clone https://github.com/Fube/homework_ws_buffoon_backed/ BackedFront
