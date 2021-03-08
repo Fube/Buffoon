d18e363b09d8964eee517251e01b3ab69e5c2138
Fube <fubeitch@gmail.com>
2021-03-07 22:18:21 -0500

    Added clone.sh

---
+++ clone.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/Fube/homework_ws_buffoon
+git clone https://github.com/Fube/homework_ws_buffoon_rating_service
+git clone https://github.com/Fube/homework_ws_buffoon_user_service
+git clone https://github.com/Fube/homework_ws_buffoon_frontend
+git clone https://github.com/Fube/homework_ws_buffoon_backed/
